ac99e4bb0b1ba79974b0fa4c91ab8e4dfbd758b6
Tao Xu <taox@fb.com>
2020-03-02 00:01:18 -0800

    Update(auto commit)

--- CIFAR10/MPSCNNHelloWorld/CIFAR10_Classifier.swift
+++ CIFAR10/MPSCNNHelloWorld/CIFAR10_Classifier.swift
@@ -16,8 +16,8 @@
      self.conv3 = nn.Conv2d(32,64,3,padding=1)
      self.pool  = nn.MaxPool2d(2,2)
      self.dropout = nn.Dropout(0.2)
-     self.fc1   = nn.Linear(64*4*4,500)
-     self.fc2   = nn.Linear(500,10)
+     self.fc1   = nn.Linear(64*4*4,128)
+     self.fc2   = nn.Linear(128,10)
  def forward(self, x):
      x = self.pool(F.relu(self.conv1(x)))
      x = self.pool(F.relu(self.conv2(x)))
