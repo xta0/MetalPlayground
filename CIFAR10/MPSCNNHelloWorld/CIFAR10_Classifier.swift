9544260e656be93ef88ec0c13048b859fabf952f
Tao Xu <taox@fb.com>
2020-03-05 15:03:00 -0800

    Update(auto commit)

--- CIFAR10/MPSCNNHelloWorld/CIFAR10_Classifier.swift
+++ CIFAR10/MPSCNNHelloWorld/CIFAR10_Classifier.swift
@@ -12,25 +12,26 @@
  class Classifier(nn.Module):
  def __init__(self):
  super().__init__()
- self.conv1 = nn.Conv2d(3,16,3,padding=1)
- self.conv2 = nn.Conv2d(16,32,3,padding=1)
- self.conv3 = nn.Conv2d(32,64,3,padding=1)
- self.pool  = nn.MaxPool2d(2,2)
- self.dropout = nn.Dropout(0.2)
- self.fc1   = nn.Linear(64*4*4,128)
- self.fc2   = nn.Linear(128,10)
+     self.conv1 = nn.Conv2d(3,16,3,padding=1)
+     self.conv2 = nn.Conv2d(16,32,3,padding=1)
+     self.conv3 = nn.Conv2d(32,64,3,padding=1)
+     self.pool  = nn.MaxPool2d(2,2)
+     self.dropout = nn.Dropout(0.2)
+     self.fc1   = nn.Linear(64*4*4,128)
+     self.fc2   = nn.Linear(128,10)
+     
  def forward(self, x):
- x = self.pool(F.relu(self.conv1(x)))
- x = self.pool(F.relu(self.conv2(x)))
- x = self.pool(F.relu(self.conv3(x)))
- #flatten the input
- x = x.view(-1,64*4*4)
- x = self.dropout(x)
- x = F.relu(self.fc1(x))
- x = self.dropout(x)
- x = F.relu(self.fc2(x))
- x = F.log_softmax(x, dim=1)
- return x
+     x = self.pool(F.relu(self.conv1(x)))
+     x = self.pool(F.relu(self.conv2(x)))
+     x = self.pool(F.relu(self.conv3(x)))
+     #flatten the input
+     x = x.view(-1,64*4*4)
+     x = self.dropout(x)
+     x = F.relu(self.fc1(x))
+     x = self.dropout(x)
+     x = F.relu(self.fc2(x))
+     x = F.log_softmax(x, dim=1)
+     return x
  */
 
 // input tensor size:
@@ -47,7 +48,7 @@ class CIFAR10_Classifier : NeuralNetwork {
     let c2id    = MPSImageDescriptor(channelFormat: .float16, width: 16, height: 16, featureChannels: 32)
     let p2id    = MPSImageDescriptor(channelFormat: .float16, width: 8, height: 8, featureChannels: 32)
     let c3id    = MPSImageDescriptor(channelFormat: .float16, width: 8, height: 8, featureChannels: 64)
-    let p3id    = MPSImageDescriptor(channelFormat: .float16, width: 4, height: 4, featureChannels: 32)
+    let p3id    = MPSImageDescriptor(channelFormat: .float16, width: 4, height: 4, featureChannels: 64)
     let fc1id   = MPSImageDescriptor(channelFormat: .float16, width: 1, height: 1, featureChannels: 128)
     let fc2id   = MPSImageDescriptor(channelFormat: .float16, width: 1, height: 1, featureChannels: 10)
     
@@ -62,18 +63,15 @@ class CIFAR10_Classifier : NeuralNetwork {
     var inputImg: MPSImage!
     
     init(device: MTLDevice, inflightBuffers: Int) {
-        pool    = MPSCNNPoolingMax(device: device, kernelWidth: 2, kernelHeight: 2)
         relu    = MPSCNNNeuronReLU(device: device, a: 0)
         softmax = MPSCNNSoftMax(device: device)
-        
         weightsLoader = { name, count in ParameterLoaderBundle2(name: name, count: count, suffix: "_W", ext: "txt") }
         biasLoader = { name, count in ParameterLoaderBundle2(name: name, count: count, suffix: "_b", ext: "txt") }
-        
         conv1 = convolution(device: device, kernel: (3,3), inChannels: 3, outChannels: 16, activation: relu, name: "conv1")
         conv2 = convolution(device: device, kernel: (3,3), inChannels: 3, outChannels: 32, activation: relu, name: "conv2")
         conv3 = convolution(device: device, kernel: (3,3), inChannels: 3, outChannels: 64, activation: relu, name: "conv3")
-        pool = maxPooling(device: device, kernel: (2,2), stride: (1,1))
-        fc1 = dense(device: device, shape: (4,4), inChannels: 16, fanOut: 128, activation: relu, name: "fc1")
+        pool = maxPooling(device: device, kernel: (2,2), stride: (2,2))
+        fc1 = dense(device: device, shape: (4,4), inChannels: 64, fanOut: 128, activation: relu, name: "fc1")
         fc2 = dense(device: device, shape: (1,1), inChannels: 128, fanOut: 10, activation: nil, name: "fc2")
         softmax         = MPSCNNSoftMax(device: device)
         outputImg       = MPSImage(device: device, imageDescriptor: fc2id)
@@ -81,23 +79,23 @@ class CIFAR10_Classifier : NeuralNetwork {
     
     func encode(commandBuffer: MTLCommandBuffer, texture: MTLTexture, inflightIndex: Int) {
         
-        let conv1Img = MPSTemporaryImage(commandBuffer: commandBuffer, imageDescriptor: fc1id)
+        let conv1Img = MPSTemporaryImage(commandBuffer: commandBuffer, imageDescriptor: c1id)
         conv1.encode(commandBuffer: commandBuffer, sourceImage: inputImg, destinationImage: conv1Img)
         
         let p1Img = MPSTemporaryImage(commandBuffer: commandBuffer, imageDescriptor: p1id)
         pool.encode(commandBuffer: commandBuffer, sourceImage: conv1Img, destinationImage: p1Img)
         
-        let conv2Img = MPSTemporaryImage(commandBuffer: commandBuffer, imageDescriptor: fc1id)
+        let conv2Img = MPSTemporaryImage(commandBuffer: commandBuffer, imageDescriptor: c2id)
         conv2.encode(commandBuffer: commandBuffer, sourceImage: p1Img, destinationImage: conv2Img)
         
         let p2Img = MPSTemporaryImage(commandBuffer: commandBuffer, imageDescriptor: p2id)
         pool.encode(commandBuffer: commandBuffer, sourceImage: conv2Img, destinationImage: p2Img)
         
-        let conv3Img = MPSTemporaryImage(commandBuffer: commandBuffer, imageDescriptor: fc1id)
+        let conv3Img = MPSTemporaryImage(commandBuffer: commandBuffer, imageDescriptor: c3id)
         conv3.encode(commandBuffer: commandBuffer, sourceImage: p2Img, destinationImage: conv3Img)
         
         let p3Img = MPSTemporaryImage(commandBuffer: commandBuffer, imageDescriptor: p3id)
-        pool.encode(commandBuffer: commandBuffer, sourceImage: conv2Img, destinationImage: p3Img)
+        pool.encode(commandBuffer: commandBuffer, sourceImage: conv3Img, destinationImage: p3Img)
         
         let fc1Img = MPSTemporaryImage(commandBuffer: commandBuffer, imageDescriptor: fc1id)
         fc1.encode(commandBuffer: commandBuffer, sourceImage: p3Img, destinationImage: fc1Img)
@@ -108,6 +106,9 @@ class CIFAR10_Classifier : NeuralNetwork {
         softmax.encode(commandBuffer: commandBuffer, sourceImage: fc2Img, destinationImage: outputImg)
         
     }
+    //expected result
+    //tensor([[8.3575e-01, 7.0725e-02, 6.0735e-02, 1.7954e-02, 1.0604e-02, 4.1041e-03,
+    //3.2909e-05, 3.2909e-05, 3.2909e-05, 3.2909e-05]])
     func fetchResult(inflightIndex: Int) -> NeuralNetworkResult<CIFAR10_Classifier.PredictionType> {
         let probabilities = outputImg.toFloatArray()
         let (maxIndex, maxValue) = probabilities.argmax()
--- CIFAR10/MPSCNNHelloWorld/ViewController.swift
+++ CIFAR10/MPSCNNHelloWorld/ViewController.swift
@@ -9,28 +9,136 @@
 import UIKit
 import MetalPerformanceShaders
 
-class ViewController: UIViewController {
-    
-    var commandQueue: MTLCommandQueue!
-    var device: MTLDevice!
+let MaxBuffersInFlight = 3   // use triple buffering
 
+extension String  {
+    var isNumber: Bool {
+        return !isEmpty && rangeOfCharacter(from: CharacterSet.decimalDigits.inverted) == nil
+    }
+}
+
+@available(iOS 11.3, *)
+class ViewController: UIViewController {
+    var commandQueue : MTLCommandQueue!
+    var device       : MTLDevice!
+    var runner       : Runner!
+    var network      : CIFAR10_Classifier!
+    var startupGroup = DispatchGroup()
+    var srcImage     : MPSImage!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        // Load default device.
+        self.view.backgroundColor = .white
+        
         device = MTLCreateSystemDefaultDevice()
+        if device == nil {
+            print("Error: this device does not support Metal")
+            return
+        }
+        srcImage = getInputImage(name: "0_6.txt")
+        commandQueue = device.makeCommandQueue()
+        startupGroup.enter()
+        createNeuralNetwork {
+            self.startupGroup.leave()
+        }
+        startupGroup.notify(queue: .main) {
+            self.predict()
+        }
+    }
+    func createNeuralNetwork(completion: @escaping () -> Void) {
         // Make sure the current device supports MetalPerformanceShaders.
         guard MPSSupportsMTLDevice(device) else {
-            print("Metal Performance Shaders not Supported on current Device")
+            print("Error: this device does not support Metal Performance Shaders")
             return
         }
         
-        // Create new command queue.
-        commandQueue = device!.makeCommandQueue()
+        runner = Runner(commandQueue: commandQueue, inflightBuffers: MaxBuffersInFlight)
+        
+        // Because it may take a few seconds to load the network's parameters,
+        // perform the construction of the neural network in the background.
+        DispatchQueue.global().async {
+            
+            timeIt("Setting up neural network") {
+                self.network = CIFAR10_Classifier(device: self.device, inflightBuffers: MaxBuffersInFlight)
+            }
+            DispatchQueue.main.async(execute: completion)
+        }
+    }
+    func predict() {
+        // Since we want to run in "realtime", every call to predict() results in
+        // a UI update on the main thread. It would be a waste to make the neural
+        // network do work and then immediately throw those results away, so the
+        // network should not be called more often than the UI thread can handle.
+        // It is up to VideoCapture to throttle how often the neural network runs.
+//        let (tests, labels) = filesInBundle()
+//        var cn = 0
+//        for i in 0..<tests.count {
+//            let label = labels[i]
+//            let fileName = tests[i]
+//            network.inputImg = getInputImage(name: fileName)!
+//            runner.predict(network: network, texture: network.inputImg.texture, queue: .main) { results in
+//                let (clz, _ ) = results.predictions[0]
+//                if clz == label {
+//                    print("True: ( output:\(clz), label:\(label) )")
+//                    cn += 1
+//                } else {
+//                    print("False: ( output:\(clz), label:\(label) )")
+//                }
+//                if i == tests.count - 1 {
+//                    print("Accuracy: \(String(format:"%.3f", Float(cn)/Float(tests.count)))")
+//                }
+//            }
+//        }
+        let inputImage: MPSImage! = getInputImage(name: "0_6.txt")
+        network.inputImg = inputImage
+        runner.predict(network: network, texture: inputImage.texture, queue: .main) { result in
+            print(result)
+        }
         
     }
     
-
-
+    func getInputImage(name:String) -> MPSImage? {
+        guard let path = Bundle.main.path(forResource: name, ofType: "") else {
+            return nil
+        }
+        if let content = try? String(contentsOfFile:path, encoding: String.Encoding.utf8) {
+            let strs = content.components(separatedBy: ",")
+            var nums: [Float] = strs.map { str -> Float in
+                let trimmed = str.replacingOccurrences(of: "\n", with: "")
+                return Float(trimmed)!
+            }
+            return MPSImage(device: device,
+                            numberOfImages: 1,
+                            width: 32,
+                            height: 32,
+                            featureChannels: 4,
+                            array: &nums,
+                            count: 32*32*4 )
+            
+        }
+        return nil;
+    }
+    
+    func filesInBundle() -> ([String], [String]) {
+        let urls = Bundle.main.paths(forResourcesOfType: "txt", inDirectory: "")
+        var tests:[String] = []
+        var labels:[String] = []
+        for path in urls {
+            if path.hasSuffix(".txt") {
+                let fileName = (path as NSString).lastPathComponent
+                let tmp = fileName.components(separatedBy: "_")
+                let name = tmp[0]
+                let label = tmp[1].components(separatedBy: ".")[0]
+                if name.isNumber {
+                    tests.append(fileName)
+                    labels.append(label)
+                }
+            }
+        }
+        return (tests,labels)
+    }
+    
+    
+    
 }
 
